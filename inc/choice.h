dd896447871dc4359bbe206aaa921287be8d52c2
WuGuommming <35882620+WuGuommming@users.noreply.github.com>
2019-06-14 00:22:39 +0800

    Add files via upload

--- inc/choice.h
+++ inc/choice.h
@@ -1,1 +1,24 @@
-1
+#include <string>
+using namespace std;
+
+class Choice
+{
+public:
+    bool Success;
+    int Ser;
+
+    void Get(QDialog dialog)
+    {
+        for(int i=0; i<1e6; ++i)
+        {
+            //TODO: get view input
+            if(dialog.*** == true)
+            {
+                Success = true;
+                Ser = ***
+                return;
+            }
+        }
+        Success = false;
+    }
+}
